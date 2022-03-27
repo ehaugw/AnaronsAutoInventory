2a60baa33a48e64d4ffd754c129cc45f5a7d0211
Eivind <ehaugw@gmail.com>
2022-03-27 20:36:47 +0200

    fix auto roll

--- AnaronsAutoInventory_Roll.lua
+++ AnaronsAutoInventory_Roll.lua
@@ -6,10 +6,10 @@ function AAI_HandleRoll(event, args)
         for _, roll_id in ipairs(GetActiveLootRollIDs()) do
             item_link = GetLootRollItemLink(roll_id)
             -- local _, _, _, quality, bindOnPickUp, canNeed, canGreed, _ = GetLootRollItemInfo(RollID)
-            for roll, keyword in ipairs(roll_types) do
+            for roll_plus_one, keyword in ipairs(roll_types) do
                 if AAI_HasTag(AAI_CleanItemLinkForDatabase(item_link), keyword) then
                     if event == "START_LOOT_ROLL" or event == "PLAYER_ENTERING_WORLD" then
-                        RollOnLoot(roll_id, roll)
+                        RollOnLoot(roll_id, roll_plus_one - 1)
                         print(string.format("Rolled %s on %s", keyword, item_link))
                     end
                 end
@@ -19,7 +19,7 @@ function AAI_HandleRoll(event, args)
         roll_id = args[1]
         roll = args[2]
         item_link = GetLootRollItemLink(roll_id)
-        if AAI_HasTag(AAI_CleanItemLinkForDatabase(item_link), roll_types[roll]) then
+        if AAI_HasTag(AAI_CleanItemLinkForDatabase(item_link), roll_types[roll + 1]) then
             if event == "START_LOOT_ROLL" or event == "PLAYER_ENTERING_WORLD" then
                 ConfirmLootRoll(roll_id, roll)
                 print(string.format("Confirmed %s on %s", keyword, item_link))
@@ -35,7 +35,7 @@ AAI_ConfirmLootRoll = ConfirmLootRoll
 function ConfirmLootRoll(roll_id, roll, ...)
     AAI_ConfirmLootRoll(roll_id, roll, ...)
     item_link = GetLootRollItemLink(roll_id)
-    if roll ~= 2 and not AAI_HasTag(item_link, roll_types[roll]) then
-        AAI_AddTag(item_link, roll_types[roll])
+    if roll ~= 1 and not AAI_HasTag(item_link, roll_types[roll + 1]) then
+        AAI_AddTag(item_link, roll_types[roll + 1])
     end
 end
