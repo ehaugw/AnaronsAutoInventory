a34ba9ae9447df96f3c73088267d2a52804237a4
Eivind <ehaugw@gmail.com>
2022-05-02 20:46:29 +0200

    fix(stat): set competing item in inventory to first of slot

--- AnaronsAutoInventory_Stat.lua
+++ AnaronsAutoInventory_Stat.lua
@@ -217,7 +217,7 @@ end
 function AAI_GetCompetingItemFromInventory(item_link, tag)
     local item_slots = AAI_GetItemSlots(item_link)
     for bag, slot, link in AAI_InventoryIterator("inventory") do
-        if AAI_HasTag(link, tag) and AAI_GetItemSlots(link) == item_slots then
+        if AAI_HasTag(link, tag) and AAI_GetItemSlots(link)[1] == item_slots[1] then
             return link
         end
     end
