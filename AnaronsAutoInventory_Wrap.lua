b1ef095ddc2f60fae695878eb4a98a2bde0f4f45
Eivind <ehaugw@gmail.com>
2023-01-18 15:59:36 +0100

    feat: create version 3.4.0 to 3.4.1 wrapper

---
+++ AnaronsAutoInventory_Wrap.lua
@@ -0,0 +1,10 @@
+GetContainerNumSlots = C_Container.GetContainerNumSlots
+GetContainerItemLink = C_Container.GetContainerItemLink
+PickupContainerItem  = C_Container.PickupContainerItem
+
+function GetContainerItemInfo(...)
+    r = C_Container.GetContainerItemInfo(...)
+    if not r then return end
+    return r.iconFileID, r.stackCount, r.isLocked, r.quality, r.isReadable, r.hasLoot, r.hyperlink, r.isFiltered, r.hasNoValue, r.itemID, r.isBound
+end
+
