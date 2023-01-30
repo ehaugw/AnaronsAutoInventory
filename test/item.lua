f9a866cfec357b80438a5a3aa5d17a424b710dbb
Eivind Haug-Warberg <eivind.haug-warberg@cgi.com>
2023-01-30 14:43:08 +0100

    test/items: include arcanite reaper

--- test/item.lua
+++ test/item.lua
@@ -1,1 +1,2 @@
-item_links_thunderfury = "\124cffff8000\124Hitem:19019::::::::70:::::\124h[Thunderfury, Blessed Blade of the Windseeker]\124h\124r"
+item_links_thunderfury          = "\124cffff8000\124Hitem:19019::::::::70:::::\124h[Thunderfury, Blessed Blade of the Windseeker]\124h\124r"
+item_links_arcanite_reaper      = "\124cff0070dd\124Hitem:12784::::::::60:::::\124h[Arcanite Reaper]\124h\124r"
