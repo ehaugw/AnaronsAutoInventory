56dfee0090809cefe84c478751c87a9fcabb22fc
Eivind Haug-Warberg <eivind.haug-warberg@cgi.com>
2023-01-31 11:56:25 +0100

    misc: resolve interpretor errors

--- AnaronsAutoInventory_Roll.lua
+++ AnaronsAutoInventory_Roll.lua
@@ -4,7 +4,7 @@ local roll_types = {"pass", "need", "greed"}
 function AAI_HandleRoll(event, args)
     if event == "START_LOOT_ROLL" or event == "PLAYER_ENTERING_WORLD" then
         for _, roll_id in ipairs(GetActiveLootRollIDs()) do
-            item_link = GetLootRollItemLink(roll_id)
+            local item_link = GetLootRollItemLink(roll_id)
             -- local _, _, _, quality, bindOnPickUp, canNeed, canGreed, _ = GetLootRollItemInfo(RollID)
             for roll_plus_one, keyword in ipairs(roll_types) do
                 if AAI_HasTag(AAI_CleanItemLinkForDatabase(item_link), keyword) then
@@ -14,12 +14,11 @@ function AAI_HandleRoll(event, args)
             end
         end
     elseif event == "CONFIRM_LOOT_ROLL" then
-        roll_id = args[1]
-        roll = args[2]
-        item_link = GetLootRollItemLink(roll_id)
+        local roll_id = args[1]
+        local roll = args[2]
+        local item_link = GetLootRollItemLink(roll_id)
         if AAI_HasTag(AAI_CleanItemLinkForDatabase(item_link), roll_types[roll + 1]) then
             ConfirmLootRoll(roll_id, roll)
-            -- print(string.format("Confirmed %s on %s", keyword, item_link))
         end
     end
 end
@@ -28,9 +27,8 @@ end
 AAI_RollOnLoot = RollOnLoot
 
 function RollOnLoot(roll_id, roll, ...)
-    item_link = GetLootRollItemLink(roll_id)
+    local item_link = GetLootRollItemLink(roll_id)
     AAI_RollOnLoot(roll_id, roll, ...)
-    -- if roll ~= 1 and not AAI_HasTag(item_link, roll_types[roll + 1]) then
     if IsShiftKeyDown() then
         for _, roll_tag in pairs(roll_types) do
             if AAI_HasTag(item_link, roll_tag) then
